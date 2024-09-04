f1307ed0f8990e91efb78ceae3e375a1b3928daa
codeantonio <135669211+codeantonio@users.noreply.github.com>
2024-09-04 13:41:22 -0300

    Add files via upload

---
+++ MarioPequeno.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Fernando
+ */
+public class MarioPequeno  implements EstadoMario{
+    
+    public void pegarCogumelo(Mario mario){
+        
+    }
+}
